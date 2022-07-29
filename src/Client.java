fc7b870db4f160993a1b917326857e9eb554bbdc
GitDemo <wjsalswo985@naver.com>
2022-07-29 09:40:17 +0900

    real

---
+++ src/Client.java
@@ -0,0 +1,8 @@
+
+public class Client {
+	String name;
+	int age;
+	char gender;
+	double weight,height,bmi;
+	String result;
+}
--- src/Project2.java
+++ src/Project2.java
@@ -3,27 +3,53 @@ import java.util.Scanner;
 public class Project2 {
 	public static void main(String[] args) {
 		Scanner sn = new Scanner(System.in);
-		System.out.print("체중을 입력하시오 : ");
-		double weight = sn.nextDouble();
-		System.out.print("신장을 입력하시오 : ");
-		int height = sn.nextInt();
-		 
-		double BMI = weight/((height*height)*0.0001);
+		Client jimin = new Client();
+		System.out.print("Name:");
+		jimin.name = sn.nextLine();
 		
-		System.out.printf("%2.1f\n",BMI);
 		
-		if(BMI>=35) {
-			System.out.println("고도 비만");
-		}else if(30<=BMI&&BMI<=34.9) {
-			System.out.println("중도 비만");
-		}else if(25<=BMI&&BMI<=29.9) {
-			System.out.println("경도 비만");
-		}else if(23<=BMI&&BMI<=24.9) {
-			System.out.println("과체중");
-		}else if(18.5<=BMI&&BMI<=22.9) {
-			System.out.println("정상");
-		}else {
-			System.out.println("저체중");
+		System.out.print("Age:");
+		jimin.age = sn.nextInt();
+		sn.nextLine();
+		
+		System.out.print("Gender(남성:m/ 여성:f):");
+		String gender = sn.nextLine();
+		while(gender.charAt(0)!='m' && gender.charAt(0)!='f'){
+			System.out.println("다시 입력하시오!:");
+			gender = sn.nextLine();
 		}
+		jimin.gender=gender.charAt(0);
+		
+		System.out.print("Weight:");
+		jimin.weight=sn.nextDouble();
+		
+		System.out.print("Height:");
+		jimin.height=sn.nextDouble();
+		
+		jimin.bmi=jimin.weight/(jimin.height*jimin.height*0.0001); 
+		double BMI = jimin.bmi;
+		
+//		System.out.print("체중을 입력하시오 : ");
+//		double weight = sn.nextDouble();
+//		System.out.print("신장을 입력하시오 : ");
+//		int height = sn.nextInt();
+//		 
+//		double BMI = weight/((height*height)*0.0001);
+//		
+		System.out.printf("당신의 BMI수치는 : %2.1f 입니다.\n",BMI);
+		
+		if(BMI>=35) jimin.result ="고도 비만";
+		else if(30<=BMI&&BMI<=34.9) {
+			jimin.result = ("중도 비만");
+		}else if(25<=BMI&&BMI<=29.9) {
+			jimin.result = ("경도 비만");
+		}else if(23<=BMI&&BMI<=24.9) {
+			jimin.result = ("과체중");
+		}else if(18.5<=BMI&&BMI<=22.9) {
+			jimin.result = ("정상");
+		}else {
+			jimin.result = ("저체중");
+		}
+		System.out.println("당신은 "+jimin.result+"입니다.");
 	}
 }
